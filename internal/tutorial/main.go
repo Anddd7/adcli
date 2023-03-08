80e4238cc6050043b5210949e5c2ac3eaf202cb1
Anddd7 <liaoad_space@sina.com>
2023-03-08 19:43:05 +0800

    hello world - 2

--- internal/tutorial/main.go
+++ internal/tutorial/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+	target := "World"
+	if name != "" {
+		target = name
+	}
+	return fmt.Sprintf("Hello, %s!", target)
 }
--- internal/tutorial/main_test.go
+++ internal/tutorial/main_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func Test_Hello(t *testing.T) {
-	got := Hello("Anddd7")
-	want := "Hello, Anddd7!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Anddd7")
+		want := "Hello, Anddd7!"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+	})
+	t.Run("saying hello to world", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
