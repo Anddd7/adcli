6544502310227e44cbba01482cc48c9b380238d3
Anddd7 <liaoad_space@sina.com>
2023-03-08 20:21:08 +0800

    interation - benchmark

--- internal/tutorial/interation/repeat.go
+++ internal/tutorial/interation/repeat.go
@@ -1,8 +1,10 @@
 package iteration
 
+const REPEAT_TIMES = 5
+
 func Repeat(s string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < REPEAT_TIMES; i++ {
 		repeated += s
 	}
 	return repeated
