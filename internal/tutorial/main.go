c52e43fa0f3abd0839738c93c218d4b51a042893
Anddd7 <liaoad_space@sina.com>
2023-03-08 19:45:51 +0800

    hello world - refactor

--- internal/tutorial/main.go
+++ internal/tutorial/main.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+const ENGLISH_HELLO_FMT = "Hello, %s!"
+
 func Hello(name string) string {
 	target := "World"
 	if name != "" {
 		target = name
 	}
-	return fmt.Sprintf("Hello, %s!", target)
+	return fmt.Sprintf(ENGLISH_HELLO_FMT, target)
 }
--- internal/tutorial/main_test.go
+++ internal/tutorial/main_test.go
@@ -7,17 +7,21 @@ func Test_Hello(t *testing.T) {
 		got := Hello("Anddd7")
 		want := "Hello, Anddd7!"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying hello to world", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// tell the test suite that this method is a helper.
+	// the line number reported will be in our function call rather than inside our test helper
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
